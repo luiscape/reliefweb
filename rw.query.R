8ef9ae70200db0bb8050d53d417578af88562118
Luis Capelo <luiscape@gmail.com>
2014-02-23 17:40:14 -0500

    Allowed for the query of 4 fields.
    Now we can query 4 fields. I made this for the fetching of the
    `body-html` data.

--- rw.query.R
+++ rw.query.R
@@ -1,6 +1,6 @@
 #### ReliefWeb Package ####
 # This script yet-to-be-package has been written with th objective of exploring
-# data from the ReliefWeb service for research purposes. 
+# data from the ReliefWeb service for research purposes.
 #
 # This work relies on ReliefWeb's API (which is still in aplha).
 # The documentation for the API is available at http://apidoc.rwlabs.org/
@@ -10,43 +10,44 @@
 #  Author: Luis Capelo | capelo@un.org | @luiscape
 
 
-rw.query <- function(type = c("report", "job", "training", "country", "disaster"),
-                     limit = c(1:1000),
+rw.query <- function(type = c("report", "job"), # These are the only two options available.
+                     limit = c(1:1000, "all"), # 'all' means retreive all metadata from a certain query.
                      country = "Syria",
                      field1 = "NA",
                      field2 = "NA",
                      field3 = "NA",
                      field4 = "NA",
-                     field5 = "NA", 
-                     debug = c(TRUE, FALSE)) {
+                     field5 = "NA",
+                     debug = FALSE) {
 
   require(jsonlite) # for reading the resulting JSON file.
   require(RCurl) # for making HTTP requests.
   require(lubridate) # for working with dates.
 
-  # Building the query structure using HTTP POST.
+  # If a limit isn't established, then it is emplied that we'll get all the data.
+  if (limit == 'NULL') { limit <- c(1000) }
+  if (limit == "all") { limit <- c(1000) }
+
+  # This URL is the base-URL for acquiring data.
   base.url <- paste("http://api.rwlabs.org/v0/",
                     type,
                     "/list",
                     "?limit=",
                     limit,
-                    sep = "") # This URL is the base-URL for acquiring data.
+                    sep = "")
 
+  # This URL is for acquiring the 'count' metadata.
   count.url <- paste("http://api.rwlabs.org/v0/",
                      type,
                      "/count",
                      "?limit=",
                      limit,
-                     sep = "") # This URL is for acquiring the 'count' metadata.
+                     sep = "")
 
   if (field1 != "NA") { query.url <- paste("&fields[include][0]=", field1, sep = "") }
-
   if (field2 != "NA") { query.url <- paste(query.url,"&fields[include][1]=", field2, sep = "") }
-
   if (field3 != "NA") { query.url <- paste(query.url,"&fields[include][2]=", field3, sep = "") }
-
   if (field4 != "NA") { query.url <- paste(query.url,"&fields[include][3]=", field4, sep = "") }
-
   if (field5 != "NA") { query.url <- paste(query.url,"&fields[include][4]=", field5, sep = "") }
 
   url <- paste(base.url, query.url,
@@ -75,24 +76,24 @@ rw.query <- function(type = c("report", "job", "training", "country", "disaster"
   count <- count$count
 
   ### Fetching the data. ###
-  if (debug == TRUE) { 
+  if (debug == TRUE) {
     x <- paste("The number of fields being fetched is: ", url, sep = "")
     print(x)
-  } 
-  
+  }
+
   query <- data.frame(fromJSON(getURLContent(url)))
-  
-  # Function to convert the nested lists into rows in the data.frame. 
+
+  # Function to convert the nested lists into rows in the data.frame.
   rw.fields <- function(df = "NA") {
-    
+
     # Counting the number of fields. Will work better with regex.
-    ifelse(field1 != 'NA', a <- 1, a <- 0)
-    ifelse(field2 != 'NA', a <- 2, a <- 1)
-    ifelse(field3 != 'NA', a <- 3, a <- 2)
-    ifelse(field4 != 'NA', a <- 4, a <- 3)
-    ifelse(field5 != 'NA', a <- 5, a <- 4)
-    
-    if (debug == TRUE) { 
+    if (field1 != "NA") { a <- 1 }
+    if (field2 != "NA") { a <- 2 }
+    if (field3 != "NA") { a <- 3 }
+    if (field4 != "NA") { a <- 4 }
+    if (field5 != "NA") { a <- 5 }
+
+    if (debug == TRUE) {
       x <- paste("This is how many fields we have: ", a, sep = "")
       print(x)
     }
@@ -103,14 +104,16 @@ rw.query <- function(type = c("report", "job", "training", "country", "disaster"
         if (i == 3) { field <- field3 }
         if (i == 4) { field <- field4 }
         if (i == 5) { field <- field5 }
-        
-        if (debug == TRUE ) { 
-         if (i == 1) { print('And they are:') } 
+
+        if (debug == TRUE ) {
+         if (i == 1) { print('And they are:') }
          print(field)
         }
-        x <- as.data.frame(list(df$data.list.fields[i]))
+        
+        # I am having a problem here where the dates are not being parsed correctly. 
+        x <- data.frame(as.list(df$data.list.fields[i]))
         df <- cbind(df, x)
-        colnames(df)[ncol(x) + 1] <- field
+#         colnames(df)[ncol(df)] <- field # adding names is confusing due to the odd order fields show up. 
     }
     df$data.list.fields <- NULL
     return(df)
@@ -118,84 +121,90 @@ rw.query <- function(type = c("report", "job", "training", "country", "disaster"
 
   query <- rw.fields(df = query)
 
-  if (debug == TRUE) { 
-    print("The column names of the data.frame are: ")
-    print(as.list(colnames(query)))
-  }
 
   # Creating a metadata data.frame.
-  meta.data <- query[1, 1:7]
-  write.csv(meta.data, file = "data/metadata.csv", row.names = FALSE)
-  
+  if (debug != TRUE) {
+    meta.data <- query[1, 1:7]
+    write.csv(meta.data, file = "data/metadata.csv", row.names = FALSE)
+  }
+
+  if (debug == TRUE) {
+    print("The 'colnames' are:")
+    print(colnames(query))
+    y <- paste("The number of rows 'query' are: ", nrow(query), sep = "")
+    print(y)
+
+    print("this is the final date row.")
+    print(query$date.created[nrow(query)])
+  }
+
   # Creating iterations to go around the limits issue.
   rw.it <- function(df = "NA") {
-    to <- query$date.created[nrow(query)]
-    final <- df
-    
-    # create progress bar
-    total <- ceiling(count/limit)
-    pb <- txtProgressBar(min = 0, max = total, style = 3)
-    
-    for (i in 2:total) {
-      Sys.sleep(0.1)
-      # update progress bar
-      setTxtProgressBar(pb, i)
+      to <- df$created[nrow(df)]
+      final <- df
       
-        # Adjusting the order of fields. 
-      if (debug == TRUE) { 
-        x <- paste("This is the last date fetched: ", query$date[nrow(query)], sep = "")
-        print(x)
+      # create progress bar
+      total <- ceiling(count/limit)
+      pb <- txtProgressBar(min = 0, max = total, style = 3)
+
+      for (i in 2:total) {
+        Sys.sleep(0.1)
+        # update progress bar
+        setTxtProgressBar(pb, i)
+
+          to <- final$created[nrow(final)]
+
+          it.url <- paste(url, "&filter[field]=date.created&filter[value][to]=", format(to, scientific = FALSE), sep = "")
+
+        if (debug == TRUE) {
+          x <- paste("This is the iteration URL: ", it.url, sep = "")
+          print(x)
+      
+          y <- paste("And this is the iteration date: ", to, sep = "")
+          print(y)
+        }
+
+          x <- data.frame(fromJSON(getURLContent(it.url)))
+
+          x <- rw.fields(df = x)
+
+          final <- rbind(final, x)
       }
-      
-        to <- final$date[nrow(final)]
-        
-        it.url <- paste(url, "&filter[field]=date.created&filter[value][to]=", format(to, scientific = FALSE), sep = "")
-        
-      if (debug == TRUE) { 
-        x <- paste("This is the iteration URL: ", it.url, sep = "")
-        print(x)
-      } 
-      
-        x <- data.frame(fromJSON(getURLContent(it.url)))
-
-        x <- rw.fields(df = x)
-      
-        final <- rbind(final, x)
-    }
-    close(pb)
+      close(pb)
     return(final)
-  } 
+  }
 
-  query <- rw.it(df = query)
-  
+query <- rw.it(df = query)
+#   if (limit == "all") { query <- rw.it(df = query) } 
+
   ## Cleaning the data.
   rw.time <- function(df = "NA") {
-    df$date.created <- df$date.created/1000 # to eliminate the miliseconds. 
-    df$date.created <- as.Date(as.POSIXct(as.numeric(df$date.created), origin = "1970-01-01"))
-    return(df) 
+    df$created <- df$created/1000 # to eliminate the miliseconds.
+    df$created <- as.Date(as.POSIXct(as.numeric(df$created), origin = "1970-01-01"))
+    return(df)
   }
 
   query <- rw.time(df = query) # to transform dates from Epoch to year-month-day.
-  
-  # Cleaning the dates. 
-  rw.clean.dates <- function (df = "NA") { 
-      x <- ymd(df$date.created) < ymd('2014-01-30')
+
+  # Cleaning the dates.
+  rw.clean.dates <- function (df = "NA") {
+      x <- ymd(df$created) < ymd('2014-01-30')
       df <- cbind(df, x)
-      x <- subset(df, df$x == 'TRUE') 
-    return(x) 
+      x <- subset(df, df$x == 'TRUE')
+    return(x)
   }
-  
+
   query <- rw.clean.dates(df = query)
-  
+
   # Cleaning useless columns.
     x <- query[8]
-    y <- query[10:12]
+    y <- query[10:ncol(query) - 1]
   query <- cbind(x,y)
-  
-  # Cleaning duplicate entries. 
+
+  # Cleaning duplicate entries.
   rw.clean.duplicates <- function(df = "NA") {
     query$duplicated <- duplicated(query)
-    x <- subset(query, query$duplicated == FALSE) 
+    x <- subset(query, query$duplicated == FALSE)
     x$duplicated <- NULL
     return(x)
   }
@@ -206,6 +215,6 @@ rw.query <- function(type = c("report", "job", "training", "country", "disaster"
 
 # Storing the resulting data in a CSV file.
   write.csv(query, file = paste("data/", country, "-", type, ".csv", sep = ""))
-  
+
   return(query)
 }
